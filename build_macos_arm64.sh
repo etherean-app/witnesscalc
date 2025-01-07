9362a1c248bb3b6ef1947fcadd8972ceea485c6b
Eduard Maximovich <edward.vstock@gmail.com>
2025-01-07 20:18:11 +0400

    add build script

---
+++ build_macos_arm64.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+if [ ! -d "depends/gmp/package_macos_arm64" ]
+then
+    echo "Build GMP for macos aarch64"
+    ./build_gmp.sh macos_arm64
+fi
+
+mkdir -p _build && cd _build
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DUSE_ASM=Off -DTARGET_PLATFORM=macos-aarch64 -DCMAKE_INSTALL_PREFIX="${PWD}/../package_macos_arm64"
+cmake --build . --target all -- -j 8
+cmake --build . --target install
